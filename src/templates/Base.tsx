7777aa168274207a5521ec004dc23090ab262b94
shashank2000 <sr22@stanford.edu>
2023-09-26 00:00:05 -0700

    letter casing

--- src/templates/Base.tsx
+++ src/templates/Base.tsx
@@ -4,7 +4,7 @@ import { Banner } from './Banner';
 import { Footer } from './Footer';
 import { Hero } from './Hero';
 import { VerticalFeatures } from './VerticalFeatures';
-import { YouTubePlayer } from './YouTubePlayer';
+import { YouTubePlayer } from './YoutubePlayer';
 
 const Base = () => (
   <div className="antialiased text-gray-600">
